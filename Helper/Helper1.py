c1464a46b46a2520550411d34b12ab969398428a
SandorMore <msaso0725@gmail.com>
2025-05-09 21:54:15 +0200

    FiboClac Done

--- Helper/Helper1.py
+++ Helper/Helper1.py
@@ -1,4 +1,28 @@
 # Convert the binary number 10101 to decimal and print it
 binary_number = "10101"
 decimal_number = int(binary_number, 2)
-print(decimal_number)
+print(4 * 1.5)
+
+import math
+
+print(a := math.sin(30) / math.sin(40))
+print(a)
+
+print(math.sin(a)**-1)
+
+
+memo = {}
+def countFib(n : int) -> int:
+    if n in memo:
+        return memo[n]
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    memo[n] = countFib(n - 1) + countFib(n - 2)
+    return memo[n]
+
+    
+print(countFib(8))
