be4abcbeb715027c2b2f1f1e252bc3b32de74d9b
SandorMore <msaso0725@gmail.com>
2025-05-10 16:25:57 +0200

    some changes

--- Helper/Helper2.py
+++ Helper/Helper2.py
@@ -1,5 +1,18 @@
-nums = [1,2,3,4,5,6,7,8,9]
+nums = [9,1,2,3,4,5,6,7,8,9,9]
 
 even_nums = [num for num in nums if num % 2 == 0]
 
 print(even_nums)
+nums.pop()
+print(nums)
+print(nums.count(9))
+
+a, *b, c = 1, 2, 3, 4, 5, 6, 7
+
+print(a, b , c)
+
+def findInfo(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print(findInfo(name="Sándor", age="30", city="Debrecen"))
