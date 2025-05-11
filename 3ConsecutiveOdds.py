494cba250668a16178679ceb463046c8a8cdfc93
SandorMore <msaso0725@gmail.com>
2025-05-11 13:27:07 +0200

    works -2

--- 3ConsecutiveOdds.py
+++ 3ConsecutiveOdds.py
@@ -2,10 +2,10 @@ def _3ConsecutiveOdds(arr: list) -> bool:
     if len(arr) < 3:
         return False
     
-    for i in range(2, len(arr) + 2):
-        if (arr[i] % 2 != 0 and arr[i - 1] % 2 != 0 and arr[i - 2] != 0):
+    for i in range(2, len(arr)):
+        if (arr[i] % 2 != 0 and arr[i - 1] % 2 != 0 and arr[i - 2] % 2 != 0):
             return True
             
     return False
 
-print(_3ConsecutiveOdds([1,3,4,5,7,9]))
+print(_3ConsecutiveOdds([1,3,4,5,7]))
