978ee3d5a916529e0922eaf5e84d28ed975c22db
Saso <msaso0725@gmail.com>
2025-05-19 08:25:57 +0200

    anp

---
+++ GoodTrinagle.py
@@ -0,0 +1,17 @@
+def tri(nums):
+    if nums[0] + nums[1] <= nums[2] or nums[0] + nums[2] <= nums[1] or nums[1] + nums[2] <= nums[0]:
+        return "none"
+    
+    
+    for i in range(1, len(nums)):
+        if nums.count(nums[i]) == 3:
+            return "equilateral"
+
+        elif nums.count(nums[i]) == 2:
+            return "isosceles"
+
+        elif nums.count(nums[i]) == 1 and nums.count(nums[i-1]) == 1:
+            return "scalene"
+
+
+print(tri([31, 1, 2]))
