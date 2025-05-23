d4d5126b95f2495c649822892add2d202e47cee0
SandorMore <msaso0725@gmail.com>
2025-05-23 21:12:42 +0200

    try

--- Helper/Tester1.py
+++ Helper/Tester1.py
@@ -1,4 +1,10 @@
 n = int(input())
-print(*range(3) *range(0,3,1) *range(10, 7 - 1, -1), sep="")
 
 print(*[i for pair in zip(range(1, 4), range(9, 6, -1)) for i in pair])
+
+a = input("What u thinkin fam? ")
+
+try:
+	print(int(a))
+except Exception as e:
+	print(f"Error: {e}")
